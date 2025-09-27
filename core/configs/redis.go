4b89178b4e659da721712a55c3cff90da6fed1ce
Romy Andre <romyandre79@gmail.com>
2025-09-27 17:30:15 +0700

    change some core folder and file

--- core/configs/redis.go
+++ core/configs/redis.go
@@ -2,21 +2,26 @@ package configs
 
 import (
 	"context"
-	"fmt"
+	"erp6-be-golang/core/helpers"
+	"strconv"
 
 	"github.com/redis/go-redis/v9"
 )
 
 func InitRedis(cfg *Config) (*redis.Client, error) {
+	DB, err := strconv.Atoi(cfg.RedisDB)
+	if err != nil {
+		helpers.IsError(err, "Redis DB", true)
+	}
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     cfg.RedisAddr,
 		Password: cfg.RedisPass,
-		DB:       cfg.RedisDB,
+		DB:       DB,
 	})
 
 	// Ping test
 	if err := rdb.Ping(context.Background()).Err(); err != nil {
-		return nil, fmt.Errorf("redis ping: %w", err)
+		helpers.IsError(err, "Redis Server", true)
 	}
-	return rdb, nil
+	return rdb, err
 }
